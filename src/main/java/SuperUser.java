9b6a36f1938feaeef7aa7a7185cf721c3bc1c1a0
daniel <dantov87@hotmail.com>
2015-03-30 13:12:57 +0200

    The first commit's message is: branch-1 -add name
    branch-1 rename last name
    
    branch-1- rename class

---
+++ src/main/java/SuperUser.java
@@ -0,0 +1,7 @@
+/**
+ * Created by dtovar on 30.3.2015.
+ */
+public class SuperUser {
+    private String name;
+    private String lastName;
+}
--- src/main/java/USer.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by dtovar on 30.3.2015.
- */
-public class User {
-}
