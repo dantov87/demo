20ba54c7c94331b344119fce1f97240e1ee6bb57
daniel <dantov87@hotmail.com>
2015-03-30 13:28:51 +0200

    branch-2 - generate getters and setters

--- src/main/java/SuperUser.java
+++ src/main/java/SuperUser.java
@@ -8,4 +8,21 @@ public class SuperUser {
     public SuperUser(){
 
     }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
